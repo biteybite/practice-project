a24ab8da53bf8f4eb6720eab1b312b88107080e3
biteybite <romancamille7@gmail.com>
2023-06-16 02:53:54 -0400

    some more shit

--- combat system.py
+++ combat system.py
@@ -47,36 +47,67 @@ def stat_roll():
 def ancestry():
     print("From what peoples do you hail?")
     race_choice = str.casefold(input("> "))
-    if race_choice == ("dwarf" or "dwarves"):
-        global player_constitution, player_strength, player_dexterity, player_race
+    global player_constitution, player_strength, player_dexterity, player_charisma, player_wisdom, player_intelligence, player_race
+    
+    if (race_choice.__contains__("dwarf" or "dwarves")):
         player_race = "dwarf"
         player_constitution += 2
         player_strength += 2
         player_dexterity -=2
-        print(f"You are a {player_race}. Your constitution is now {player_constitution}. Your strength is now {player_strength}. Your dexterity is now {player_dexterity}")
+        print(f"You are a {player_race}. Your constitution is now {player_constitution}. Your strength is now {player_strength}. Your dexterity is now {player_dexterity}.")
         print("Press y to confirm your choice. Press n to choose a different origin.")
         choice = input("> ")
         if choice == "y":
-            print("Confirmed, friend dwarf.")
+            print("Confirmed, you are a dwarf.")
         else:
             player_constitution -= 2
             player_strength -= 2
             player_dexterity +=2
             ancestry()
+            
+    elif (race_choice.__contains__("elf" or "elves") and not race_choice.__contains__("half")):
+        player_race = "elf"
+        player_constitution -= 2
+        player_dexterity += 2
+        player_intelligence += 2
+        print(f"You are an {player_race}. Your constitution is now {player_constitution}. Your dexterity is now {player_dexterity}. Your intelliegence is now {player_intelligence}.")
+        print("Press y to confirm your choice. Press n to choose a different origin.")
+        choice = input("> ")
+        if choice == "y":
+            print("Confirmed, you are an elf.")
+        else:
+            player_constitution += 2
+            player_dexterity -= 2
+            player_intelligence -= 2
+            ancestry()
+    
+    elif (race_choice.__contains__("elf" or "elves") and race_choice.__contains__("half")):
+        player_race = "half-elf"
+        player_charisma += 2
+        print(f"You are a {player_race}. Your charisma is now {player_charisma}.")
+        print("Press y to confirm your choice. Press n to choose a different origin.")
+        choice = input("> ")
+        if choice == "y":
+            print("Confirmed, you are a half-elf.")
+        else:
+            player_charisma -= 2
+            ancestry()
+
     else:
         print("Never heard of them. Try again.")
+        player_race = ()
         ancestry()
         
 
 
 def character_creation():
     print("What is your name?")
-    global player_name
+    global player_name, player_strength, player_constitution, player_dexterity, player_charisma, player_intelligence, player_wisdom
     player_name = input("> ")
+    print(f"{player_name}, huh? Interesting name.")
 
     print("Let's roll your stats.")
     input("> ")
-    global player_strength, player_constitution, player_dexterity, player_charisma, player_intelligence, player_wisdom
     player_strength = stat_roll()
     print(f"You have a strength score of {player_strength}")
     player_constitution = stat_roll()
@@ -89,7 +120,9 @@ def character_creation():
     print(f"You have a intelligence score of {player_intelligence}")
     player_wisdom = stat_roll()
     print(f"You have a wisdom score of {player_wisdom}")
+    
     ancestry()
-
+    
+    
 
 character_creation()
